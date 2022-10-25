b13612414e8794db4259bea0de4f4968312f23af
Nida Arslan <miinidars@gmail.com>
2022-10-26 01:58:38 +0300

    tahmin

---
+++ app/src/main/java/com/example/sayitahminet/SonucActivity.java
@@ -0,0 +1,46 @@
+package com.example.sayitahminet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SonucActivity extends AppCompatActivity {
+    private ImageView imageViewSonuc;
+    private TextView textViewSonuc;
+    private Button buttonTekrar;
+
+    private boolean sonuc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sonuc);
+        imageViewSonuc=findViewById(R.id.imageViewSonuc);
+        textViewSonuc=findViewById(R.id.textViewSonuc);
+        buttonTekrar=findViewById(R.id.buttonTekrar);
+
+        sonuc=getIntent().getBooleanExtra("sonuc",false);
+        if (sonuc){
+            textViewSonuc.setText("KAZANDINIZ");
+            imageViewSonuc.setImageResource(R.drawable.mutlu_resim);
+        }else{
+            textViewSonuc.setText("KAYBETTİNİZ");
+            imageViewSonuc.setImageResource(R.drawable.mutsuz_resim);
+
+        }
+
+
+        buttonTekrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SonucActivity.this,TahminActivity.class));
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/sayitahminet/TahminActivity.java
@@ -0,0 +1,69 @@
+package com.example.sayitahminet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class TahminActivity extends AppCompatActivity {
+    private TextView textViewKalanHak, textViewYardim;
+    private EditText editTextGirdi;
+    private Button buttonTahmin;
+
+    private int rastgeleSayi;
+    private int sayac =5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tahmin);
+        textViewKalanHak=findViewById(R.id.textViewKalanHak);
+        textViewYardim=findViewById(R.id.textViewYardim);
+        editTextGirdi=findViewById(R.id.editTextGirdi);
+        buttonTahmin=findViewById(R.id.buttonTahmin);
+
+        Random r = new Random();
+
+        rastgeleSayi=r.nextInt(101); //0-100
+        Log.e("Rastgele Sayı",String.valueOf(rastgeleSayi));
+
+        buttonTahmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sayac=sayac-1;
+                int tahmin = Integer.parseInt(editTextGirdi.getText().toString());
+                if (tahmin==rastgeleSayi){
+                    Intent i=new Intent(TahminActivity.this,SonucActivity.class);
+                    i.putExtra("sonuc",true);
+                    startActivity(i);
+                    finish();
+                    return;
+                }
+                if (tahmin>rastgeleSayi){
+                    textViewYardim.setText("Azalt");
+                    textViewKalanHak.setText("Kalan Hak :"+sayac);
+                }
+                if (tahmin<rastgeleSayi){
+                    textViewYardim.setText("Arttır");
+                    textViewKalanHak.setText("Kalan Hak :"+sayac);
+                }
+                if (sayac==0){
+                    Intent i=new Intent(TahminActivity.this,SonucActivity.class);
+                    i.putExtra("sonuc",false);
+                    startActivity(i);
+                    finish();
+                }
+
+                editTextGirdi.setText("");
+
+            }
+        });
+
+    }
+}
